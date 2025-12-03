cba8d92f2bab962945397dd6020ab55578e082c2
Duvan Vasquez <29andrey10@gmail.com>
2025-12-02 21:29:34 -0600

    add  new UI elements and styles

--- login_calidad_software/src/libs/toast/client.ts
+++ login_calidad_software/src/libs/toast/client.ts
@@ -1,4 +1,16 @@
 import { toastAdapter } from "./adapter";
+import type { ToasterProps } from "sonner";
+
+export const toastConfig: {
+  position: ToasterProps["position"];
+  defaultDuration: number;
+  richColors: boolean;
+} = {
+  position: "top-right",
+  defaultDuration: 3000,
+  richColors: true,
+};
+
 
 export const notify = {
   success: (msg: string) => toastAdapter.success(msg),
@@ -6,7 +18,6 @@ export const notify = {
   info: (msg: string) => toastAdapter.info(msg),
   warning: (msg: string) => toastAdapter.warning(msg),
   loading: (msg: string) => toastAdapter.loading(msg),
-
   promise: <T>(promise: Promise<T>, msgs: any) =>
     toastAdapter.promise(promise, msgs),
 };
